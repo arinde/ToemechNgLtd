1e7f5f580ba22008e4acaec76c1aa405c882fd6f
arinde <arindevictor8@gmail.com>
2025-09-22 18:14:39 +0100

    feat/added the whatsapp icon to lead to whatsapp

--- src/components/WhatsappFAB.tsx
+++ src/components/WhatsappFAB.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 const WhatsAppFAB = () => {
   return (
     <a
-      href="https://wa.me/2348033923394"
+      href="https://wa.me/23480234444635?text=Hello%20Toemech%2C%20I%27d%20like%20to%20get%20a%20quote."
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-600 hover:bg-green-700 text-white rounded-full p-4 shadow-lg z-50 transition-all animate-pulse block md:hidden"
